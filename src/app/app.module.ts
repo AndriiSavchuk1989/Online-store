49feeb42326f87c3fcf4b328ea3c06409b5c7d36
MancunianRed11 <andrey.m.savchuk@gmail.com>
2019-02-19 00:24:44 +0200

    Online store v0.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
 import { ProductsComponent } from './products/products.component';
 import { AppRoutingModule } from './app-routing.module';
 import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component';
+import {TermsConditionsComponent} from './terms-conditions/terms-conditions.component';
+import {CartComponent} from './cart/cart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent,
     FooterComponent,
     ProductsComponent,
-    PrivacyPolicyComponent
+    PrivacyPolicyComponent,
+    TermsConditionsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
