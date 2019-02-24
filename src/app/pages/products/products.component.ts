dfd0c353eb57289231aae95f71d4358e19aae7ef
MancunianRed11 <andrey.m.savchuk@gmail.com>
2019-02-24 23:34:00 +0200

    Online store v0.5 (products fixed)

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+import { Product } from '../../model/product';
+import { CartService } from '../../service/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent { }
+export class ProductsComponent implements OnInit {
+  public products: Array<Product>;
+  private sub;
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.load();
+  }
+  load = () => {
+    this.sub = this.productService.getProducts('../../../assets/db.json');
+    this.products = this.sub;
+  }
+  addToCart = (product) => {
+    this.cartService.addToCart({product, quantity: 1});
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+}
