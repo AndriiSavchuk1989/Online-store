065f93fc64caea607a65cf293d17c7d2874a0c58
MancunianRed11 <andrey.m.savchuk@gmail.com>
2019-02-26 00:10:51 +0200

    Online store v0.6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import {ProductsComponent} from './pages/products/products.component';
 import {PrivacyPolicyComponent} from './pages/static/privacy-policy/privacy-policy.component';
 import {TermsConditionsComponent} from './pages/static/terms-conditions/terms-conditions.component';
 import {LoginComponent} from './pages/auth/login/login.component';
+import {CartPageComponent} from './pages/cart/cart.component';
+import {ProductComponent} from './pages/product/product.component';
 
 const appRoutes: Routes = [
   {path: 'products', component: ProductsComponent},
   {path: 'privacy-policy', component: PrivacyPolicyComponent},
   {path: 'terms-conditions', component: TermsConditionsComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'cart', component: CartPageComponent},
+  {path: 'product', component: ProductComponent}
 ];
 
 @NgModule({
